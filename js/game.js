219262f85da0318ee50b2e6d3f73740efbced117
crl5608 <161096185+crl5608@users.noreply.github.com>
2024-09-18 16:02:45 -0400

    Update game.js
    added sumthree function

--- js/game.js
+++ js/game.js
@@ -14,6 +14,20 @@ function rideHeight() {
         document.getElementById("rideResult").innerHTML = "You are tall enough to ride!"}
     }
 
+function sumThreeNumbers() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var num3 = document.getElementById("num3").value;
+
+    if (num1 === '' || num2 === '' || num3 === '') {
+        document.getElementById("sumResult").innerHTML = "<span class='error-message'>Please enter numeric values in all fields.</span>";
+        return;
+    }
+    
+    var sum = Number(num1) + Number(num2) + Number(num3);
+    
+    document.getElementById("sumResult").innerHTML = "The sum is: " + sum; 
+}
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var player = {
